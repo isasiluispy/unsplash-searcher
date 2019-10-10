11c65be264e8a74ec976739c58eea03a73aca56c
Luis Isasi <luisi@oktana.io>
2019-10-10 14:02:58 -0300

    event handling

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 
 function App() {
   return (
-    <div>
+    <div className="ui container" style={{'marginTop': '10px'}}>
       <SearchBar/>
     </div>
   );
